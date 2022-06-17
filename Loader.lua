df39286439f4fe017c1029a0461021c8b1a072f5
blinx <103583498+blinx-co@users.noreply.github.com>
2022-06-17 18:43:43 +0100

    Update and rename Library.lua to Loader.lua

---
+++ Loader.lua
@@ -0,0 +1,21 @@
+--[[
+    Open Script Hub Project by Novaline
+    
+    https://github.com/Novaline-Development/Open-Script-Hub
+
+    Anyone can make scripts for any game and commit them to our repository by sending a PR.
+    Staff will review your PR and accept it if we think your script should be in OSH.
+    You will be credited in our ReadMe file & can add your credits on the GUI you made for the script.
+]]--
+
+repeat
+    wait()
+until game:IsLoaded()
+
+local script = game:HttpGet('https://raw.githubusercontent.com/Novaline-Development/Open-Script-Hub/main/GuiMain.lua',true)
+
+if script == "404" then
+    warn("Script Not Found!")
+else
+    loadstring(script)
+end
